7d67e5050baea14929e9219652be0cbb6fbea196
paladox <paladox@users.noreply.github.com>
2026-03-29 00:21:40 +0000

    Update ServicesManagerTest.php

--- tests/phpunit/Unit/ServicesManagerTest.php
+++ tests/phpunit/Unit/ServicesManagerTest.php
@@ -2,6 +2,7 @@
 
 namespace Onoi\CallbackContainer\Tests;
 
+use PHPUnit\Framework\TestCase;
 use Onoi\CallbackContainer\ServicesManager;
 use Onoi\CallbackContainer\CallbackContainerFactory;
 
@@ -14,7 +15,7 @@ use Onoi\CallbackContainer\CallbackContainerFactory;
  *
  * @author mwjames
  */
-class ServicesManagerTest extends \PHPUnit_Framework_TestCase {
+class ServicesManagerTest extends TestCase {
 
 	private $servicesManager;
 
